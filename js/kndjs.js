6b16f79a932875e549d9069cfb611b7e83e5f055
PEACH <116077782+peach3848@users.noreply.github.com>
2024-10-22 23:34:05 +0800

    跨年新年倒计时js

--- js/kndjs.js
+++ js/kndjs.js
@@ -70,7 +70,7 @@ class Snow {
 new Snow();
 var stop = false;
 function show_runtime() {
-  var newDay = '2025/1/29 00:00:00';
+  var newDay = '2025/1/1 00:00:00';
   var countDate = new Date(newDay);
   var now = new Date().getTime();
   gap = countDate - now;
